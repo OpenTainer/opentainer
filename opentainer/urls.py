87c605cbd6240786759928715b2f6b1d43db6f1f
Sambuddha Basu <sambuddhabasu1@gmail.com>
2014-12-25 01:45:51 +0000

    Added requirements.txt

--- opentainer/urls.py
+++ opentainer/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
         # url(r'^$', 'mysite.views.home', name='home'),
         # url(r'^blog/', include('blog.urls')),
         url(r'', include('django.contrib.auth.urls')),
-        url(r'^accounts/', include('registration.backends.default.urls')),
+#        url(r'^accounts/', include('registration.backends.default.urls')),
         url(r'^admin/', include(admin.site.urls)),
         url(r'^web/', include('web.urls')),
         )
